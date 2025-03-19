a53b2a7e0c04e32b8f31a3783318b9abd02e1ab9
vamshi <vd5323@srmist.edu.in>
2025-03-19 11:50:47 +0530

    sample1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -12,14 +12,9 @@ const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-// Sample route (modify as needed)
-router.get("/getUserData", (req, res) => {
-  res.json({ message: "User data fetched successfully!" });
-});
-
 router.post("/login", loginController);
 router.post("/register", registerController);
-router.post("/getUserData", authMiddleware, authController);
+router.get("/getUserData", authMiddleware, authController);
 router.put("/updateProfile", authMiddleware, updateProfile);
 router.put("/updateTeacherProfile", authMiddleware, updateTeacherProfile);
 router.get("/searchTeachers", authMiddleware, searchTeachers);
